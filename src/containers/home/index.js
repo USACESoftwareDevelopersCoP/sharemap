2f0188f1ff4566a7bd1a6daaa7fa36245a36d00d
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2019-08-13 15:20:48 -0400

    edit homepage

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import { connect } from 'redux-bundler-react';
 
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.renderActions = this.renderActions.bind(this);
+    this.renderMessage = this.renderMessage.bind(this);
+  }
+
+  renderMessage(){
+    const {
+      authIsLoggedIn,
+      authUsername
+    } = this.props;
+
+    if(authIsLoggedIn){
+      return `Welcome ${authUsername}!`
+    }else{
+      return `Click "Login" to get started!`
+    }
+  }
+
+  renderActions(){
+    const { 
+      authIsLoggedIn, 
+      doAuthLogin,
+      doAuthLogout
+    } = this.props;
+
+    if(authIsLoggedIn){
+      return (
+        <button 
+          onClick={ doAuthLogout } 
+          className="btn btn-primary"
+        >Logout</button>
+      )
+    }else{
+      return (
+        <button 
+          onClick={ doAuthLogin } 
+          className="btn btn-success"
+        >Login</button>
+      )
+    }
+  }
+
   render(){
     return (
       <div className="container">
         <div className="card bg-light m-5">
           <div className="card-body">
-            <h2>Welcome to @corpsmap</h2>
-            <p>This is gonna be sweet!</p>
+            <h2>Welcome to Sharemap</h2>
+            <div className="float-right">
+              {
+                this.renderActions()
+              }
+            </div>
+            <div>
+              {
+                this.renderMessage()
+              }
+            </div>
           </div>
         </div>
       </div>
@@ -15,4 +68,10 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+export default connect(
+  'doAuthLogin',
+  'doAuthLogout',
+  'selectAuthIsLoggedIn',
+  'selectAuthUsername',
+  Home
+);
